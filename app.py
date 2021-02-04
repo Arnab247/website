3173ec7479b14917a9e374918d21a3432f14dde0
Arnab247 <arnab.mandal247@gmail.com>
2021-02-04 15:25:10 -0600

    init

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def hello(name):
             return redirect(url_for('biology30S'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(debug=False, host='0.0.0.0')
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
