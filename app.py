a11e76f5ba4a8072017b079320fc8bc28a2352c3
Arnab247 <arnab.mandal247@gmail.com>
2021-02-03 20:35:41 -0600

    init

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template('home.html')
+
 @app.route("/<name>")
 def hello(name):
     name = name
-    return render_template('home.html', hi=name)
+    return render_template('welcome-home.html', hi=name)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
