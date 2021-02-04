0776f36c1e008796c61c5b0233e79572f3ba534d
Arnab247 <arnab.mandal247@gmail.com>
2021-02-04 14:18:10 -0600

    init

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 def open_file(grade, subject):
     infile = open('files/{}/{}.txt'.format(grade, subject), 'r')
     allofit = infile.readlines()
-    splitdata = allofit[0].split("! SUBJECT ! ! SUBJECT ! !")
-    return splitdata
+    data = []
+    for n in range(len(allofit)):
+        data.append(allofit[n])
+    return data
 
 def find_unit(data):
     x = len(data)
@@ -29,8 +31,22 @@ def biology30S():
         return render_template('grade-11/biology.html', question="Test Yourself!", answer="Click one of the options below...")
     else:
         text = open_file("11", "biology")
-        text = find_unit(text)
-        text = find_question(text)
+        print(text)
+        if request.form.get('1') == 'Homeostasis and Wellness':
+            text = find_question(text[0])
+        elif request.form.get('2') == 'Digestion and Nutrition':
+            text = find_question(text[1])
+        elif request.form.get('3') == 'Biochemistry':
+            text = find_question(text[2])
+        elif request.form.get('4') == 'Transportation and Respiration':
+            text = find_question(text[3])
+        elif request.form.get('5') == 'Excretion and Waste Management':
+            text = find_question(text[4])
+        elif request.form.get('6') == 'Protection and Control':
+            text = find_question(text[5])
+        else:
+            text = find_unit(text)
+            text = find_question(text)
         return render_template('grade-11/biology.html', question=text[0], answer=text[1])
 
 @app.route("/<name>")
